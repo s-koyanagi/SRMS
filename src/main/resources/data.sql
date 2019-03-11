f11c1eb936bfc25be9f13a04ab7dc082d5174ce5
s-koyanagi <kysh19931216@gmail.com>
2019-03-12 00:02:25 +0900

    Adding Schema and Data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,15 @@
-INSERT INTO users(id,name,password,created_at) VALUES('dev@demo.co.jp','山田太郎','$2a$10$5ltUX/.ogJRHrbwn9hG2sOd8Y1krWn8mhYWbBnpCYSSjSCIoTdAke',current_timestamp);
+INSERT INTO users(id,name,password,created_at)
+VALUES('dev@demo.co.jp','山田太郎','$2a$10$5ltUX/.ogJRHrbwn9hG2sOd8Y1krWn8mhYWbBnpCYSSjSCIoTdAke',current_timestamp);
+INSERT INTO seminars(title,overview,started_at,closed_at,created_at)
+VALUES('第99回 開発技術セミナー 〜SpringBoot特集〜',
+       '今年1発目となる第17回 開発技術セミナーの開催日程が決まりましたので、ご案内いたします！！<br><br>今回テーマを決めるにあたり、<br><br>ロシア出張の報告もしたいし、、、<br>機械学習を活用した実証実験も絶対おもしろいし、、、<br>実案件でのブロックチェーンの話も興味深いし、、、<br><br>という感じで、お届けしたい(おもしろい)内容がたくさんありまして、<br>悩んだ結果、もう今回は特別に3テーマをぎゅっと盛り込みます！！<br><br>ITエンジニアとしては、興味を惹く内容ばかりだと思いますので、<br>是非足を運んでいただけると幸いです。',
+       '2019-01-01 12:34:00',
+       '2019-01-01 14:34:00',
+       current_timestamp);
+INSERT INTO speakers(seminar_id,name,theme,started_at,closed_at,created_at)
+VALUES(1,
+       '小柳匠平',
+       '誰もが知らない私の世界',
+       '18:15:00',
+       '18:45:00',
+       current_timestamp);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,3 +7,36 @@ CREATE TABLE IF NOT EXISTS users(
     deleted_at TIMESTAMP ,
     PRIMARY KEY(id)
 );
+CREATE TABLE IF NOT EXISTS seminars(
+    seminar_id identity NOT NULL,
+    title TEXT NOT NULL,
+    overview TEXT NOT NULL,
+    started_at TIMESTAMP NOT NULL,
+    closed_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP ,
+    deleted_at TIMESTAMP ,
+    PRIMARY KEY(seminar_id)
+);
+CREATE TABLE IF NOT EXISTS applicants(
+    applicant_id identity NOT NULL,
+    seminar_id integer NOT NULL,
+    name TEXT NOT NULL,
+    mail_address TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP ,
+    deleted_at TIMESTAMP ,
+    PRIMARY KEY(applicant_id)
+);
+CREATE TABLE IF NOT EXISTS speakers(
+    speaker_id identity NOT NULL,
+    seminar_id integer NOT NULL,
+    name TEXT NOT NULL,
+    theme TEXT NOT NULL,
+    started_at TIME NOT NULL,
+    closed_at TIME NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP ,
+    deleted_at TIMESTAMP ,
+    PRIMARY KEY(speaker_id)
+);
