905472ff8938190005ac21918c03e06d848001f8
s-koyanagi <kysh19931216@gmail.com>
2019-03-18 00:43:16 +0900

    Extend the IndexController(entry)

--- src/main/java/com/example/srms/web/controller/IndexController.java
+++ src/main/java/com/example/srms/web/controller/IndexController.java
@@ -1,7 +1,7 @@
 package com.example.srms.web.controller;
 
 import com.example.srms.service.SeminarService;
-import com.example.srms.web.form.IndexForm;
+import com.example.srms.web.form.EntryForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,21 +10,28 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping({"","/","/index"})
+@RequestMapping({"/index"})
 public class IndexController {
 
     @Autowired
     SeminarService seminarService;
 
     @ModelAttribute
-    public IndexForm initForm(){
-        return new IndexForm();
+    public EntryForm entryForm(){
+        return new EntryForm();
     }
 
     @RequestMapping(value={""}, method= RequestMethod.GET)
     public ModelAndView index(ModelAndView mv){
         mv.addObject("seminar",seminarService.findAcceptingSeminar());
         mv.setViewName("index/index");
+        return mv;
+    }
+
+    @RequestMapping(value="/entry", method = RequestMethod.GET)
+    public ModelAndView receptionForm(ModelAndView mv){
+        mv.addObject("seminar_id",seminarService.findAcceptingSeminar().getSeminar_id());
+        mv.setViewName("index/entry");
         return mv;
     }
 
