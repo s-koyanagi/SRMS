f045a88b6dedc6660ec893e2daed5e1f6ddb3802
s-koyanagi <kysh19931216@gmail.com>
2019-03-19 01:03:08 +0900

    Exclude the reception function

--- src/main/java/com/example/srms/web/controller/IndexController.java
+++ src/main/java/com/example/srms/web/controller/IndexController.java
@@ -1,10 +1,8 @@
 package com.example.srms.web.controller;
 
 import com.example.srms.service.SeminarService;
-import com.example.srms.web.form.EntryForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,11 +14,6 @@ public class IndexController {
     @Autowired
     SeminarService seminarService;
 
-    @ModelAttribute
-    public EntryForm entryForm(){
-        return new EntryForm();
-    }
-
     @RequestMapping(value={""}, method= RequestMethod.GET)
     public ModelAndView index(ModelAndView mv){
         mv.addObject("seminar",seminarService.findAcceptingSeminar());
@@ -28,11 +21,5 @@ public class IndexController {
         return mv;
     }
 
-    @RequestMapping(value="/entry", method = RequestMethod.GET)
-    public ModelAndView receptionForm(ModelAndView mv){
-        mv.addObject("seminar_id",seminarService.findAcceptingSeminar().getSeminar_id());
-        mv.setViewName("index/entry");
-        return mv;
-    }
 
 }
