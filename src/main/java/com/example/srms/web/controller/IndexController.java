b52afe99298245c3fe000ee8217d2ab6a3ab93e9
s-koyanagi <kysh19931216@gmail.com>
2019-04-07 01:57:49 +0900

    Adding Error Handling

--- src/main/java/com/example/srms/web/controller/IndexController.java
+++ src/main/java/com/example/srms/web/controller/IndexController.java
@@ -3,13 +3,15 @@ package com.example.srms.web.controller;
 
 import com.example.srms.web.form.LoginForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping({"/index"})
+@RequestMapping({"","/","/index"})
 public class IndexController {
 
     // Init login form
@@ -25,13 +27,15 @@ public class IndexController {
         return mv;
     }
 
-
-//    @RequestMapping(value={""}, method= RequestMethod.GET)
-//    public ModelAndView index(ModelAndView mv){
-//        mv.addObject("seminar",seminarService.findAcceptingSeminar());
-//        mv.setViewName("index/index");
-//        return mv;
-//    }
+    // Authentication error handler
+    @RequestMapping(value = {"/login_error"}, method = RequestMethod.POST)
+    public ModelAndView loginFailure(@ModelAttribute @Validated LoginForm form, BindingResult result, ModelAndView mv) {
+        if(!result.hasErrors()) {
+            mv.addObject("errorMessage", "ログイン情報が間違っています");
+        }
+        mv.setViewName("index/index");
+        return mv;
+    }
 
 
 }
