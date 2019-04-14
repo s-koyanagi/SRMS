8335f25a9ea16cbd5d9a257524639c437b9d1a05
s-koyanagi <kysh19931216@gmail.com>
2019-04-14 23:56:39 +0900

    Adding registration method

--- src/main/java/com/example/srms/web/controller/SignUpController.java
+++ src/main/java/com/example/srms/web/controller/SignUpController.java
@@ -1,6 +1,9 @@
 package com.example.srms.web.controller;
 
+import com.example.srms.domain.dto.UserInfoDto;
 import com.example.srms.web.form.SignUpForm;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +14,21 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping({"/signup"})
 public class SignUpController {
 
-    @ModelAttribute
-    public SignUpForm signUpForm(){return new SignUpForm();}
+    @Autowired
+    ModelMapper modelMapper;
 
     @RequestMapping(value={"/","/index"},method= RequestMethod.GET)
     public ModelAndView index(ModelAndView mv){
+        mv.addObject("signUpForm",new SignUpForm());
         mv.setViewName("signup/index");
+        return mv;
+    }
+
+    @RequestMapping(value={"/work"},method=RequestMethod.POST)
+    public ModelAndView registration(ModelAndView mv, @ModelAttribute SignUpForm signUpForm){
+        UserInfoDto userInfoDto = new UserInfoDto();
+        userInfoDto = modelMapper.map(signUpForm,userInfoDto.getClass());
+
         return mv;
     }
 }
