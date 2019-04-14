6c7e6e5621720384ec5a090d2915a9dfb3a0c473
s-koyanagi <kysh19931216@gmail.com>
2019-04-15 00:39:46 +0900

    Adding SignUpService And Fix registration method (SignUpController)

---
+++ src/main/java/com/example/srms/service/SignUpService.java
@@ -0,0 +1,25 @@
+package com.example.srms.service;
+
+import com.example.srms.domain.dao.UserDao;
+import com.example.srms.domain.dto.UserInfoDto;
+import com.example.srms.domain.entity.User;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SignUpService {
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Autowired
+    UserDao userDao;
+
+    public int userRegistration(UserInfoDto userInfoDto){
+        User user = new User();
+
+        //***Passwordハッシュ化ロジック追加***
+
+        user = modelMapper.map(userInfoDto,user.getClass());
+        return userDao.insertUser(user);
+    }
+}
--- src/main/java/com/example/srms/web/controller/SignUpController.java
+++ src/main/java/com/example/srms/web/controller/SignUpController.java
@@ -1,6 +1,7 @@
 package com.example.srms.web.controller;
 
 import com.example.srms.domain.dto.UserInfoDto;
+import com.example.srms.service.SignUpService;
 import com.example.srms.web.form.SignUpForm;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,9 @@ public class SignUpController {
     @Autowired
     ModelMapper modelMapper;
 
+    @Autowired
+    SignUpService signUpService;
+
     @RequestMapping(value={"/","/index"},method= RequestMethod.GET)
     public ModelAndView index(ModelAndView mv){
         mv.addObject("signUpForm",new SignUpForm());
@@ -28,7 +32,14 @@ public class SignUpController {
     public ModelAndView registration(ModelAndView mv, @ModelAttribute SignUpForm signUpForm){
         UserInfoDto userInfoDto = new UserInfoDto();
         userInfoDto = modelMapper.map(signUpForm,userInfoDto.getClass());
-
+        userInfoDto.setName(signUpForm.getFirstName()+" "+signUpForm.getLastName());
+        if(signUpService.userRegistration(userInfoDto)==0){
+            mv.addObject("errorMessage","入力されたESQIDは既に登録済みです");
+            mv.setViewName("signup/index");
+            return mv;
+        }
+        mv.addObject("signupContents", userInfoDto);
+        mv.setViewName("signup/confirm");
         return mv;
     }
 }
