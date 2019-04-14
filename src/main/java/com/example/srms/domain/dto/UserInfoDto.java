3dde8dc36506bdb32a2a418adca4571c05d8f979
s-koyanagi <kysh19931216@gmail.com>
2019-04-14 23:57:04 +0900

    Adding UserInfoDto

---
+++ src/main/java/com/example/srms/domain/dto/UserInfoDto.java
@@ -0,0 +1,14 @@
+package com.example.srms.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserInfoDto {
+
+    private String esqId;
+    private String firstName;
+    private String lastName;
+    private String password;
+}
