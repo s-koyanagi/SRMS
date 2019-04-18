c9ac521e3e18be4cfe3586b78a1877da88ae3e2c
s-koyanagi <kysh19931216@gmail.com>
2019-04-19 01:10:13 +0900

    Add mapping settings (When not one to one)

--- src/main/java/com/example/srms/config/AppConfig.java
+++ src/main/java/com/example/srms/config/AppConfig.java
@@ -1,6 +1,11 @@
 package com.example.srms.config;
 
+import com.example.srms.domain.dto.UserInfoDto;
+import com.example.srms.web.form.SignUpForm;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+import org.modelmapper.spi.MappingContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +14,19 @@ public class AppConfig {
 
     @Bean
     public ModelMapper modelMapper(){
-        return new ModelMapper();
+        ModelMapper mapper = new ModelMapper();
+        // FirstNameとLastNameを結合してFullNameにマッピングする設定
+        mapper.addMappings(new PropertyMap<SignUpForm, UserInfoDto>() {
+            @Override
+            protected void configure() {
+                using(new Converter<SignUpForm, String>(){
+                    public String convert(MappingContext<SignUpForm, String> context) {
+                        return context.getSource().getFirstName() + " "
+                                + context.getSource().getLastName();
+                    }
+                }).map(source, destination.getFullName());
+            }
+        });
+        return mapper;
     }
 }
