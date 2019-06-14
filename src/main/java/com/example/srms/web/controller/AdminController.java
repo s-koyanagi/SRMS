ec251c04d2ab58017c8016d2001dc99140eb3122
Shohei Koyanagi <kysh19931216@gmail.com>
2019-06-14 18:35:15 +0900

    fix method

--- src/main/java/com/example/srms/web/controller/AdminController.java
+++ src/main/java/com/example/srms/web/controller/AdminController.java
@@ -1,6 +1,8 @@
 package com.example.srms.web.controller;
 
 import com.example.srms.domain.entity.User;
+import com.example.srms.service.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +13,12 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/admin")
 public class AdminController {
 
+    @Autowired
+    DashboardService dashboardService;
     // View login page
     @RequestMapping(value={""}, method= RequestMethod.GET)
     public ModelAndView index(ModelAndView mv, @AuthenticationPrincipal User userDetails){
+        mv.addObject("values",dashboardService.getAnalyticsData());
         return mv;
     }
 }
