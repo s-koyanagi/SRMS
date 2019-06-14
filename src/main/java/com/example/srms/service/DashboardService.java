65ece91e1a879f96d38adee30c327872b6ad89ae
Shohei Koyanagi <kysh19931216@gmail.com>
2019-06-14 18:34:05 +0900

    fix business logic

--- src/main/java/com/example/srms/service/DashboardService.java
+++ src/main/java/com/example/srms/service/DashboardService.java
@@ -1,9 +1,7 @@
 package com.example.srms.service;
 
-import com.example.srms.domain.dao.GuestDao;
 import com.example.srms.domain.dao.SeminarDao;
 import com.example.srms.domain.dto.DashboardDTO;
-import com.example.srms.domain.dto.GuestDTO;
 import com.example.srms.domain.dto.SeminarDTO;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
@@ -11,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 public class DashboardService {
@@ -21,47 +21,31 @@ public class DashboardService {
     @Autowired
     SeminarDao seminarDao;
     @Autowired
-    GuestDao guestDao;
-    @Autowired
     ModelMapper modelMapper;
+
+    final int ACCEPTING_FLAG = 1;
+    final int TARGET_INDEX_NUMBER = 0;
 
     public DashboardDTO getAnalyticsData(){
         DashboardDTO dashboardDTO = new DashboardDTO();
-        final int ACCEPTING_FLAG = 1;
-        final int TARGET_INDEX_NUMBER = 0;
+
         List <SeminarDTO> seminarDTOList = modelMapper.map(seminarDao.selectAllSeminarAndGuestInfo(), new TypeToken<List<SeminarDTO>>() {}.getType());
-        List <GuestDTO> guestDTOList = modelMapper.map(guestDao.selectAllGuest(), new TypeToken<List<GuestDTO>>() {}.getType());
 
+        Supplier<Integer> getAcceptingNumberOfGuest = () -> {
+            SeminarDTO acceptingSeminar = seminarDTOList.stream()
+                                                        .filter((seminar) -> seminar.getAcceptingFlag() == ACCEPTING_FLAG)
+                                                        .collect(Collectors.collectingAndThen(toList(), list -> list.get(TARGET_INDEX_NUMBER)));
+            return acceptingSeminar.getNumberOfGuest();
+        };
 
+        Supplier <Integer> getNumberOfHighestGuest = () -> {
+            Optional<SeminarDTO> numberOfHighestGuestSeminar = seminarDTOList.stream()
+                                                                            .max((seminar1, seminar2) -> ((Integer)seminar1.getNumberOfGuest()).compareTo(seminar2.getNumberOfGuest()));
+            return numberOfHighestGuestSeminar.get().getNumberOfGuest();
+        };
 
-//        Supplier<Integer> getNumberOfGuest= () -> {
-//            List<SeminarDTO> acceptingSeminar = seminarDTOList.stream()
-//                                                              .filter((seminar) -> seminar.getAcceptingFlag()==ACCEPTING_FLAG)
-//                                                              .collect(Collectors.toList());
-//
-//            int numberOfGuest = (int)guestDTOList.stream()
-//                                                 .filter((guest) -> guest.getSeminarId()==acceptingSeminar.get(TARGET_INDEX_NUMBER).getSeminarId())
-//                                                 .count();
-//            return numberOfGuest;
-//        };
-//
-//        Supplier <Integer> getNumberOfHigestGuest= () -> {
-//            Map.Entry<Integer, Long> highestSeminar = null;
-//            Map<Integer, Long> groupingGuest = guestDTOList.stream().collect(Collectors.groupingBy(GuestDTO::getSeminarId, Collectors.counting()));
-//            groupingGuest.forEach((key, value) -> { if(highestSeminar == null || value > highestSeminar.getValue()){
-//                                                        highestSeminar.set
-//                                                    }
-//                                                  });
-//
-//            return highestGuestList.size();
-//        };
-
-
-
-
-
-
-
+        dashboardDTO.setNumberOfGuest(getAcceptingNumberOfGuest.get());
+        dashboardDTO.setNumberOfHighestGuest(getNumberOfHighestGuest.get());
 
         return dashboardDTO;
     }
