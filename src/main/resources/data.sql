b6c803cf604c10b83b9c28230e56543d0389300d
Shohei Koyanagi <kysh19931216@gmail.com>
2019-06-17 19:43:57 +0900

    fix insert data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
 INSERT INTO users(esq_id,name,role,password,created_at)
 VALUES('lk9999','山田 太郎','ADMIN','$2a$10$5ltUX/.ogJRHrbwn9hG2sOd8Y1krWn8mhYWbBnpCYSSjSCIoTdAke',current_timestamp)
 ,('lk8888','鈴木 一郎','USER','$2a$10$5ltUX/.ogJRHrbwn9hG2sOd8Y1krWn8mhYWbBnpCYSSjSCIoTdAke',current_timestamp)
-,('lk4649','鈴木 一郎','USER','$2a$10$5ltUX/.ogJRHrbwn9hG2sOd8Y1krWn8mhYWbBnpCYSSjSCIoTdAke',current_timestamp)
-,('lk4648','鈴木 一郎','USER','$2a$10$5ltUX/.ogJRHrbwn9hG2sOd8Y1krWn8mhYWbBnpCYSSjSCIoTdAke',current_timestamp)
-,('lk4643','鈴木 一郎','USER','$2a$10$5ltUX/.ogJRHrbwn9hG2sOd8Y1krWn8mhYWbBnpCYSSjSCIoTdAke',current_timestamp)
-,('lk4645','鈴木 一郎','USER','$2a$10$5ltUX/.ogJRHrbwn9hG2sOd8Y1krWn8mhYWbBnpCYSSjSCIoTdAke',current_timestamp);
+,('lk4649','西野 哲也','USER','$2a$10$5ltUX/.ogJRHrbwn9hG2sOd8Y1krWn8mhYWbBnpCYSSjSCIoTdAke',current_timestamp)
+,('lk4648','大野 聡','USER','$2a$10$5ltUX/.ogJRHrbwn9hG2sOd8Y1krWn8mhYWbBnpCYSSjSCIoTdAke',current_timestamp)
+,('lk4643','福田 将司','USER','$2a$10$5ltUX/.ogJRHrbwn9hG2sOd8Y1krWn8mhYWbBnpCYSSjSCIoTdAke',current_timestamp)
+,('lk4645','斎藤 五郎','USER','$2a$10$5ltUX/.ogJRHrbwn9hG2sOd8Y1krWn8mhYWbBnpCYSSjSCIoTdAke',current_timestamp);
 INSERT INTO seminars(title,overview,started_at,closed_at,created_at,accepting_flag,version)
 VALUES('第99回 開発技術セミナー 〜SpringBoot特集〜','とくにございません','2019-01-01 12:34:00','2019-01-01 14:34:00',current_timestamp,1,1)
 ,('第98回 開発技術セミナー 〜くらうど特集〜','特になし','2019-01-01 12:34:00','2019-01-01 14:34:00',current_timestamp,0,1);
@@ -18,6 +18,6 @@ VALUES(1,'lk4649','好きな格言を教えてください',current_timestamp)
 ,(1,'lk4645','わかりません',current_timestamp)
 ,(1,'lk4648','好きな食べ物',current_timestamp)
 ,(1,'lk4643','好きな飲み物',current_timestamp)
-,(2,'lk4643','好きな飲み物',current_timestamp)
-,(2,'lk4648','好きな飲み物',current_timestamp)
-,(2,'lk4649','好きな飲み物',current_timestamp);
+,(2,'lk4643','好きなスペイン料理',current_timestamp)
+,(2,'lk4648','好きな銘柄牛',current_timestamp)
+,(2,'lk4649','好きな銘柄豚',current_timestamp);
