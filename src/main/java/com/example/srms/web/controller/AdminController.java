1608554c277de34df0e3b5867f6dbc1ff46b5cdb
Shohei Koyanagi <kysh19931216@gmail.com>
2019-06-20 20:21:17 +0900

    Adding showSeminarList method

--- src/main/java/com/example/srms/web/controller/AdminController.java
+++ src/main/java/com/example/srms/web/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.example.srms.web.controller;
 
 import com.example.srms.domain.entity.User;
 import com.example.srms.service.DashboardService;
+import com.example.srms.service.SeminarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -16,11 +17,22 @@ public class AdminController {
     @Autowired
     DashboardService dashboardService;
 
-    @RequestMapping(value={""}, method= RequestMethod.GET)
+    @Autowired
+    SeminarService seminarService;
+
+    @RequestMapping(value={""}, method = RequestMethod.GET)
     public ModelAndView index(ModelAndView mv, @AuthenticationPrincipal User userDetails){
         mv.addObject("analyticsData",dashboardService.getAnalyticsData());
         mv.addObject("userDetails",userDetails);
         mv.setViewName("admin/index");
+        return mv;
+    }
+
+    @RequestMapping(value={"/seminarlist"}, method = RequestMethod.GET)
+    public ModelAndView showSeminarList(ModelAndView mv, @AuthenticationPrincipal User userDetails){
+        mv.addObject("seminarList",seminarService.getAllSeminarList());
+        mv.setViewName("admin/seminarList");
+        mv.addObject("userDetails",userDetails);
         return mv;
     }
 }
