28f633ee1a6f6835ddaf83df5c8c5a05f78f9d81
s-koyanagi <kysh19931216@gmail.com>
2019-06-22 23:28:04 +0900

    minor fix

--- src/main/resources/static/js/seminar.js
+++ src/main/resources/static/js/seminar.js
@@ -2,9 +2,8 @@ function getSeminarInfo(seminarId){
     axios
         .get("http://localhost:8080/srms/admin/getseminar/"+seminarId)
         .then(response => {
-            const element = '<h3>'+response.data.seminarDTO.title+'</h3>';
             let target = document.getElementById('seminarDetails');
-            target.innerHTML(element);
+            target.innerHTML = '<h3>'+response.data.title+'</h3>';
         })
         .catch(error => {
             UIkit.modal.alert('取得失敗!')
