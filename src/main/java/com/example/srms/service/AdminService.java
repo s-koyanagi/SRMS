00d80fecfa5a6c5a24dbc12ff2d22f528b626607
Shohei Koyanagi <kysh19931216@gmail.com>
2019-06-25 20:12:01 +0900

    Adding AdminService

---
+++ src/main/java/com/example/srms/service/AdminService.java
@@ -0,0 +1,31 @@
+package com.example.srms.service;
+
+import com.example.srms.domain.dao.GuestDao;
+import com.example.srms.domain.dao.SeminarDao;
+import com.example.srms.domain.dto.GuestDTO;
+import com.example.srms.domain.dto.SeminarDTO;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class AdminService {
+
+    @Autowired
+    SeminarDao seminarDao;
+    @Autowired
+    GuestDao guestDao;
+    @Autowired
+    ModelMapper modelMapper;
+
+    public SeminarDTO getSeminarInfo(int seminarId){
+        SeminarDTO seminarDTO = modelMapper.map(seminarDao.selectBySeminarId(seminarId),SeminarDTO.class);
+        return seminarDTO;
+    }
+
+    public List<GuestDTO> getGuestList(int seminarId){
+        List<GuestDTO> guestDTOList = modelMapper.map(guestDao.selectGuestListBySeminarId(seminarId), new TypeToken<List<GuestDTO>>() {}.getType());
+        return guestDTOList;
+    }
+}
