7c5f7c862f47e2bc1632044b972b978c53085843
Shohei Koyanagi <kysh19931216@gmail.com>
2019-07-02 18:07:55 +0900

    minor fix

--- src/main/java/com/example/srms/service/AdminService.java
+++ src/main/java/com/example/srms/service/AdminService.java
@@ -7,9 +7,11 @@ import com.example.srms.domain.dto.SeminarDTO;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class AdminService {
 
     @Autowired
