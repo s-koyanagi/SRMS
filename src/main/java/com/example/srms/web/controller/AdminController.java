1617456b38369783ecd6b1506d83b33d1521b0c4
Shohei Koyanagi <kysh19931216@gmail.com>
2019-07-02 18:08:39 +0900

    Add method and minor fix

--- src/main/java/com/example/srms/web/controller/AdminController.java
+++ src/main/java/com/example/srms/web/controller/AdminController.java
@@ -1,13 +1,10 @@
 package com.example.srms.web.controller;
 
-import com.example.srms.domain.dto.GuestDTO;
-import com.example.srms.domain.dto.SeminarDTO;
-import com.example.srms.domain.entity.Seminar;
 import com.example.srms.domain.entity.User;
 import com.example.srms.service.AdminService;
 import com.example.srms.service.DashboardService;
 import com.example.srms.service.SeminarService;
-import org.modelmapper.ModelMapper;
+import com.example.srms.service.SpeakerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -29,6 +26,9 @@ public class AdminController {
     @Autowired
     AdminService adminService;
 
+    @Autowired
+    SpeakerService speakerService;
+
     @RequestMapping(value={""}, method = RequestMethod.GET)
     public ModelAndView index(ModelAndView mv, @AuthenticationPrincipal User userDetails){
         mv.addObject("analyticsData",dashboardService.getAnalyticsData());
@@ -38,17 +38,18 @@ public class AdminController {
     }
 
     @RequestMapping(value={"/seminarlist"}, method = RequestMethod.GET)
-    public ModelAndView showSeminarList(ModelAndView mv, @AuthenticationPrincipal User userDetails){
+    public ModelAndView seminarList(ModelAndView mv, @AuthenticationPrincipal User userDetails){
         mv.addObject("seminarList",seminarService.getAllSeminarList());
         mv.setViewName("admin/seminarList");
         mv.addObject("userDetails",userDetails);
         return mv;
     }
 
-    @RequestMapping(value={"/getseminar/{seminarId}"}, method = RequestMethod.GET)
-    public ModelAndView showSeminarGuestDetails(ModelAndView mv, @PathVariable("seminarId") int seminarId){
-        mv.addObject("seminarInfo", adminService.getSeminarInfo(seminarId));
-        mv.addObject("guestInfo", adminService.getGuestList(seminarId));
+    @RequestMapping(value={"/seminardetails/{seminarId}"}, method = RequestMethod.GET)
+    public ModelAndView seminarDetails(ModelAndView mv, @PathVariable("seminarId") int seminarId){
+        mv.addObject("seminar", adminService.getSeminarInfo(seminarId));
+        mv.addObject("speakerList", speakerService.findSpeaker(seminarId));
+        mv.addObject("guestList", adminService.getGuestList(seminarId));
         mv.setViewName("admin/seminarDetails");
         return mv;
     }
