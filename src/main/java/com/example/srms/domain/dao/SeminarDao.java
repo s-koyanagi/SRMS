19298b7b01dc46de1ea31eccdc3b2cd4fbb0a870
Shohei Koyanagi <kysh19931216@gmail.com>
2019-07-10 17:59:35 +0900

    add update method

--- src/main/java/com/example/srms/domain/dao/SeminarDao.java
+++ src/main/java/com/example/srms/domain/dao/SeminarDao.java
@@ -1,9 +1,11 @@
 package com.example.srms.domain.dao;
 
+import com.example.srms.domain.dto.SeminarDTO;
 import com.example.srms.domain.entity.Seminar;
 import com.example.srms.domain.entity.SeminarGuest;
 import org.seasar.doma.Dao;
 import org.seasar.doma.Select;
+import org.seasar.doma.Update;
 import org.seasar.doma.boot.ConfigAutowireable;
 
 import java.util.List;
@@ -20,5 +22,8 @@ public interface SeminarDao {
 
     @Select
     Seminar selectBySeminarId(int seminarId);
+
+    @Update
+    int updateBySeminarId(SeminarDTO seminar);
 
 }
--- src/main/java/com/example/srms/domain/dao/SpeakerDao.java
+++ src/main/java/com/example/srms/domain/dao/SpeakerDao.java
@@ -1,8 +1,11 @@
 package com.example.srms.domain.dao;
 
+import com.example.srms.domain.dto.SpeakerDTO;
 import com.example.srms.domain.entity.Speaker;
+import org.seasar.doma.BatchUpdate;
 import org.seasar.doma.Dao;
 import org.seasar.doma.Select;
+import org.seasar.doma.Update;
 import org.seasar.doma.boot.ConfigAutowireable;
 
 import java.util.List;
@@ -14,4 +17,6 @@ public interface SpeakerDao {
     @Select
     List<Speaker> selectBySeminarId(int seminarId);
 
+    @BatchUpdate
+    int updateBySpeakerId(List<SpeakerDTO> speakerDTOList);
 }
