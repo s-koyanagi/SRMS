bc272cda86dcd141d20319222d897b7abf54ab36
Shohei Koyanagi <kysh19931216@gmail.com>
2019-07-18 20:53:09 +0900

    add new method

--- src/main/java/com/example/srms/service/AdminService.java
+++ src/main/java/com/example/srms/service/AdminService.java
@@ -6,11 +6,13 @@ import com.example.srms.domain.dao.SpeakerDao;
 import com.example.srms.domain.dto.GuestDTO;
 import com.example.srms.domain.dto.SeminarDTO;
 import com.example.srms.domain.dto.SpeakerDTO;
+import com.example.srms.domain.entity.ImmutableSeminar;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -35,13 +37,22 @@ public class AdminService {
         return guestDTOList;
     }
 
-    public boolean registration (SeminarDTO seminarDTO, List<SpeakerDTO> speakerDTOList){
+    public boolean registerEditInfo (SeminarDTO seminarDTO, List<SpeakerDTO> speakerDTOList){
         seminarDTO.setEventDate(seminarDTO.getEventDate().replaceAll("日","").replaceAll("\\p{InCjkUnifiedIdeographs}","-"));
         seminarDTO.setStartedDateTime(seminarDTO.getEventDate()+" "+seminarDTO.getStartedTime()+":00");
         seminarDTO.setClosedDateTime(seminarDTO.getEventDate()+" "+seminarDTO.getClosedTime()+":00");
 
         seminarDao.updateBySeminarId(seminarDTO);
         speakerDao.updateBySpeakerId(speakerDTOList);
+        return true;
+    }
+
+    public boolean registerCreateInfo(SeminarDTO seminarDTO, List<SpeakerDTO> speakerDTOList){
+        Date date = new Date();
+        seminarDTO.setEventDate(seminarDTO.getEventDate().replaceAll("日","").replaceAll("\\p{InCjkUnifiedIdeographs}","-"));
+        ImmutableSeminar seminar = new ImmutableSeminar(null,seminarDTO.getTitle(),"test",seminarDTO.getEventDate()+" "+seminarDTO.getStartedTime()+":00",
+                seminarDTO.getEventDate()+" "+seminarDTO.getClosedTime()+":00", date,null,null,1,1);
+        ImmutableSeminar result = seminarDao.insert(seminar).getEntity();
         return true;
     }
 }
