5d04f97fbb67366195c3c5a04113fdde447b6d4e
Shohei Koyanagi <kysh19931216@gmail.com>
2019-07-23 20:41:22 +0900

    fix query

--- src/main/resources/META-INF/com/example/srms/domain/dao/SeminarDao/selectAllSeminarAndGuestInfo.sql
+++ src/main/resources/META-INF/com/example/srms/domain/dao/SeminarDao/selectAllSeminarAndGuestInfo.sql
@@ -1,32 +1,28 @@
 SELECT
-b.seminar_id
-,b.title
-,b.overview
-,b.started_at
-,b.closed_at
-,b.created_at
-,b.updated_at
-,b.deleted_at
-,b.accepting_flag
-,b.version
-,COUNT(a.guest_id) AS number_of_guest
+a.seminar_id
+,a.title
+,a.overview
+,a.started_at
+,a.closed_at
+,a.created_at
+,a.updated_at
+,a.deleted_at
+,a.accepting_flag
+,a.version
+,coalesce(b.number_of_guest,0) as number_of_guest
 FROM
-guests a
+seminars a
 LEFT JOIN
-seminars b
-ON
-a.seminar_id = b.seminar_id
-AND b.deleted_at IS NULL
+(SELECT
+seminar_id
+,count(*) as number_of_guest
+FROM
+guests
 WHERE
-a.deleted_at IS NULL
+deleted_at is null
 GROUP BY
-b.seminar_id
-,b.title
-,b.overview
-,b.started_at
-,b.closed_at
-,b.created_at
-,b.updated_at
-,b.deleted_at
-,b.accepting_flag
-,b.version
+seminar_id) b
+ON
+a.seminar_id=b.seminar_id
+WHERE
+a.deleted_at is null
